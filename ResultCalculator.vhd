021f13486098d672fe60bae9f5a21f08f00eaef2
Davi Correia <davicorreiajr@gmail.com>
2017-10-28 19:02:48 -0200

    Definied inputs and outputs

--- ResultCalculator.vhd
+++ ResultCalculator.vhd
@@ -3,6 +3,10 @@ use ieee.std_logic_1164.all;
 
 entity ResultCalculator is
 	port(
+		clock: in std_logic;
+		playerOneCards: in std_logic_vector(59 downto 0);
+		playerTwoCards: in std_logic_vector(59 downto 0);
+		result: out integer
 	);
 end ResultCalculator;
 
