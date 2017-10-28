5a5eb6ea920b8ce6b298bc9a3caf7c471f9d53ad
Davi Correia <davicorreiajr@gmail.com>
2017-10-28 17:29:30 -0200

    Created base file to ControlUnit

---
+++ Exemplos/ControlUnit.vhd
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity ControlUnit is
+	port(
+	);
+end ControlUnit;
+
+architecture arch of ControlUnit is
+
+	signal 
+
+begin
+	process ()
+	begin
+
+	end process;
+
+end arch ; -- archControlUnit
