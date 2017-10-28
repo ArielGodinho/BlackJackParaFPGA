59907c28e50636c2ad8fffb0140b3298481dc80c
Davi Correia <davicorreiajr@gmail.com>
2017-10-28 18:58:13 -0200

    Created base file ResultCalculator

---
+++ ResultCalculator.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity ResultCalculator is
+	port(
+	);
+end ResultCalculator;
+
+architecture arch of ResultCalculator is
+
+	signal 
+
+begin
+
+end arch;
