1251b28a177063c358394c5521d049f918d69950
Davi Correia <davicorreiajr@gmail.com>
2017-11-27 09:57:05 -0200

    Created TerminalInterface and TopLevelEntity

---
+++ TerminalInterface.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+entity TerminalInterface is
+	port(
+		clock                : in  std_logic;
+		reset                : in  std_logic
+	);
+end TerminalInterface;
+
+architecture arch of TerminalInterface is
+	
+begin
+
+end arch;
---
+++ TopLevelEntity.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+entity TopLevelEntity is
+	port(
+		clock                : in  std_logic;
+		reset                : in  std_logic
+	);
+end TopLevelEntity;
+
+architecture arch of TopLevelEntity is
+	
+begin
+
+end arch;
